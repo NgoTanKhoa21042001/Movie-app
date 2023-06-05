b9e52988fca9b3204927ef69d0965f1a29f941ed
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-06-05 10:29:08 +0700

    call api by click on tab week or day

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -1,9 +1,39 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
 import React from "react";
+import { useState } from "react";
 import "./switchtabs.scss";
-const SwitchTabs = () => {
-  return <div>Switch Tab</div>;
+const SwitchTabs = ({ onTabChange, data }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [left, setLeft] = useState(0);
+
+  const activeTab = (tab, index) => {
+    // độ dài di chuyển
+    setLeft(index * 100);
+
+    setTimeout(() => {
+      setSelectedTab(index);
+    }, 300);
+    onTabChange(tab, index);
+  };
+  return (
+    <div className="switchingTabs">
+      <div className="tabItems">
+        {data.map((tab, index) => (
+          <span
+            key={index}
+            className={`tabItem ${selectedTab === index ? "active" : ""}`}
+            onClick={() => activeTab(tab, index)}
+          >
+            {tab}
+          </span>
+        ))}
+        <span className="movingBg" style={{ left }} />
+      </div>
+    </div>
+  );
 };
 
 export default SwitchTabs;
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,15 +1,22 @@
 /* eslint-disable no-unused-vars */
 
 import React from "react";
+import { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
 import "./trending.scss";
 const Trending = () => {
+  const [endpoint, setEndpoint] = useState("day");
+  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Day" ? "day" : "week");
+  };
   return (
     <div className="carouselSection">
       <ContentWrapper>
         <span className="carouselTitle">Trending</span>
-        <SwitchTabs />
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
     </div>
   );
