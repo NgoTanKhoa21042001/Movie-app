081f6bb1e8148930ea1defd5c6c13f49cd029e79
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-07-11 11:12:35 +0700

    change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
 import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+// const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
 };
